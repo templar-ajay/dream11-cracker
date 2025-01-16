336352c2dc34471f2804600bf082f70b3e8d1205
Ajay Kumar <templar.ajay@gmail.com>
2025-01-16 18:24:27 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`p-2 ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
